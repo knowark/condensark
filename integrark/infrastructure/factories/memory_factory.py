63ca0f3195b2442460acef66820793aae2de4dad
Esteban Echeverry <eecheverry@nubark.com>
2019-09-28 18:20:42 -0500

    Add methods to memory_factory

--- integrark/infrastructure/factories/memory_factory.py
+++ integrark/infrastructure/factories/memory_factory.py
@@ -1,3 +1,5 @@
+from ...application.services import QueryService, StandardQueryService
+from ...application.coordinators import ExecutionCoordinator
 from ..core import Config
 from .factory import Factory
 
@@ -5,3 +7,10 @@ from .factory import Factory
 class MemoryFactory(Factory):
     def __init__(self, config: Config) -> None:
         self.config = config
+
+    def standard_query_service(self) -> StandardQueryService:
+        return StandardQueryService()
+
+    def execution_coordinator(
+            self, query_service: QueryService) -> ExecutionCoordinator:
+        return ExecutionCoordinator(query_service)
