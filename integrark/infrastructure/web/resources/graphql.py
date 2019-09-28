a7f039758ffabde60c34a752558d4448435b4ba2
Esteban Echeverry <eecheverry@nubark.com>
2019-09-28 12:33:11 -0500

    Add graphql post endpoint

--- integrark/infrastructure/web/resources/graphql.py
+++ integrark/infrastructure/web/resources/graphql.py
@@ -7,3 +7,7 @@ class GraphqlResource(web.View):
     @template('playground.html')
     async def get(self):
         return {'version': '0.1.0'}
+
+    async def post(self):
+        body = await self.request.text()
+        return web.Response(text=f"BODY: {body}")
--- tests/infrastructure/web/test_api.py
+++ tests/infrastructure/web/test_api.py
@@ -10,10 +10,19 @@ async def test_root(app) -> None:
     assert 'Integrark' in await response.text()
 
 
-async def test_get_graphql(app) -> None:
+async def test_graphql_get(app) -> None:
     response = await app.get('/graphql')
 
     content = await response.text()
 
     assert response.status == 200
     assert 'GraphQL Playground' in await response.text()
+
+
+async def test_graphql_post(app) -> None:
+    response = await app.post('/graphql', data='{query{hero}}')
+
+    content = await response.text()
+
+    assert response.status == 200
+    assert '{query{hero}}' in await response.text()
