73d8e6a8c6baa1409a8520dc83e504f94dd4f616
Esteban Echeverry <eecheverry@nubark.com>
2019-09-28 17:57:04 -0500

    Use query service in executor coordinator

--- integrark/application/coordinators/execution_coordinator.py
+++ integrark/application/coordinators/execution_coordinator.py
@@ -1,11 +1,12 @@
 from typing import Dict, Any
+from ..services import QueryService
 
 
 class ExecutionCoordinator:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, query_service: QueryService) -> None:
+        self.query_service = query_service
 
     async def execute(self, query: str) -> Dict[str, Any]:
-        result = {'data': query}
+        result = await self.query_service.run(query)
 
         return result
--- tests/application/coordinators/test_query_coordinator.py
+++ tests/application/coordinators/test_query_coordinator.py
@@ -1,10 +1,12 @@
 from pytest import fixture
+from integrark.application.services import StandardQueryService
 from integrark.application.coordinators import ExecutionCoordinator
 
 
 @fixture
 def execution_coordinator() -> ExecutionCoordinator:
-    return ExecutionCoordinator()
+    query_service = StandardQueryService({'data': {'name': 'value'}})
+    return ExecutionCoordinator(query_service)
 
 
 def test_execution_coordinator_instantiation(
@@ -16,4 +18,6 @@ async def test_execution_coordinator_execute(
         execution_coordinator: ExecutionCoordinator) -> None:
 
     result = await execution_coordinator.execute('{query {name}}')
+
     assert isinstance(result, dict)
+    assert result == {'data': {'name': 'value'}}
