0dc6fbc3bdd75fed48470393e18ad8a0799d0796
eecheverry <eecheverry@nubark.com>
2019-10-18 14:35:32 -0500

    Define dataloader load and fetch methods

--- integrark/infrastructure/query/graphql/dataloader.py
+++ integrark/infrastructure/query/graphql/dataloader.py
@@ -1,19 +1,38 @@
 from abc import ABC, abstractmethod
-from asyncio import Task
-from typing import Union, List, Callable, Any, NamedTuple
+from asyncio import Future, get_event_loop, ensure_future
+from typing import Union, List, Callable, Any, NamedTuple, Awaitable
 
-Id = Union[str, int]
 
-FetchFunction = Callable[[List[Id]], List[Any]]
+FetchFunction = Callable[[List[str]], Awaitable[List[Any]]]
 
 
 class DataLoader(ABC):
     def __init__(self):
+        self.loop = get_event_loop()
         self.queue: List[Loader] = []
 
+    def load(self, id: str) -> Awaitable[Any]:
+        future = self.loop.create_future()
+        self.queue.append(Loader(id, future))
+        if len(self.queue) == 1:
+            self._schedule_dispatch()
+        return future
+
     @abstractmethod
-    def fetch(self) -> List[Any]:
+    async def fetch(self, ids: List[str]) -> List[Any]:
         """Fetch method to be implemented by subclasses"""
+
+    def _schedule_dispatch(self):
+        self.loop.call_soon(ensure_future, self._dispatch())
+
+    async def _dispatch(self):
+        queue = self.queue
+        self.queue = []
+
+        ids = [item.id for item in queue]
+        values = await self.fetch(ids)
+        for item, value in zip(queue, values):
+            item.future.set_result(value)
 
 
 class StandardDataLoader(DataLoader):
@@ -21,10 +40,10 @@ class StandardDataLoader(DataLoader):
         super().__init__()
         self.fetch_function = fetch_function
 
-    def fetch(self):
-        return self.fetch_function()
+    async def fetch(self, ids: List[str]) -> List[Any]:
+        return await self.fetch_function(ids)
 
 
 class Loader(NamedTuple):
-    id: Id
-    future: Task
+    id: str
+    future: Future
--- tests/infrastructure/query/graphql/test_dataloader.py
+++ tests/infrastructure/query/graphql/test_dataloader.py
@@ -1,23 +1,54 @@
+from typing import List, Awaitable, Any, Union
+from asyncio import Future, isfuture, sleep
 from pytest import fixture, raises
-from integrark.infrastructure.query.graphql import DataLoader
+from integrark.infrastructure.query.graphql import (
+    DataLoader, StandardDataLoader)
 
 
 def test_dataloader_methods():
     abstract_methods = DataLoader.__abstractmethods__
-
     assert 'fetch' in abstract_methods
 
 
-# @fixture
-# def dataloader() -> DataLoader:
-#     return DataLoader()
+def standard_dataloader() -> DataLoader:
+    async def dummy_fetch(ids: List[str]) -> List[Any]:
+        return [{'id': id, 'value': f'Response: {id}'} for id in ids]
+
+    return StandardDataLoader(dummy_fetch)
 
 
-# def test_loader(dataloader):
-#     assert loader.type == 'Query'
+async def test_dataloader_queue():
+    dataloader = standard_dataloader()
+
+    future_1 = dataloader.load('1')
+    future_2 = dataloader.load('2')
+    future_3 = dataloader.load('3')
+
+    assert isfuture(future_1)
+    assert isfuture(future_2)
+    assert isfuture(future_3)
+
+    assert len(dataloader.queue) == 3
 
 
-# def test_loader_default_resolver(loader):
-#     resolver = loader.resolve('any')
+async def test_dataloader_load():
+    dataloader = standard_dataloader()
 
-#     assert resolver(None, None) == []
+    future_1 = dataloader.load('1')
+    future_2 = dataloader.load('2')
+
+    assert len(dataloader.queue) == 2
+    assert future_1.done() is False
+    assert future_2.done() is False
+
+    await sleep(0.01)
+
+    assert len(dataloader.queue) == 0
+    assert future_1.done() is True
+    assert future_2.done() is True
+
+    result_1 = await future_1
+    result_2 = await future_2
+
+    assert result_1 == {'id': '1', 'value': f'Response: 1'}
+    assert result_2 == {'id': '2', 'value': f'Response: 2'}
