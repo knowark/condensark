28cbc8dac6584b3b00cf96f43447c92772bb79cf
eecheverry <eecheverry@nubark.com>
2019-11-18 12:11:05 -0500

    Fix tools test. Check Authorizer

--- tests/tools/test_tools.py
+++ tests/tools/test_tools.py
@@ -4,5 +4,4 @@ from integrark import tools
 def test_tools():
     assert tools.Solution is not None
     assert tools.DataLoader is not None
-    assert tools.StandardDataLoader is not None
-    assert tools.parse_domain is not None
+    assert tools.Authorizer is not None
