37f21ef54771cd6d70b5f1a97e26cb05cda772ce
Esteban Echeverry <eecheverry@knowark.com>
2021-04-29 22:44:04 -0500

    Implement many_to_many fetch in joiner

--- integrark/core/common/dataloader/joiner.py
+++ integrark/core/common/dataloader/joiner.py
@@ -17,6 +17,8 @@ class Joiner:
         fetch = self._many_to_one_fetch
         if self.join and self.target:
             fetch = self._one_to_many_fetch
+        if self.join and self.link:
+            fetch = self._many_to_many_fetch
 
         return StandardDataLoader(fetch, context)
 
@@ -32,3 +34,37 @@ class Joiner:
                 getattr(item, self.target), []).append(item)
 
         return [index.get(id_, []) for id_ in ids]
+
+    async def _many_to_many_fetch(self, ids: List[str]):
+        assert self.link and self.source and self.target
+        joints = await self.link.search([(self.source, 'in', ids)])
+
+        index: Dict = {}
+        for joint in filter(None, joints):  # type: ignore
+            index.setdefault(
+                getattr(joint, self.source), []).append(joint)
+
+        links = [index.get(id_, []) for id_ in ids]
+
+        ids_index: Dict = {}
+        target_ids = set()
+        for item_list in links:
+            for item in item_list:
+                source_id = getattr(item, self.source)
+                target_id = getattr(item, self.target)
+                ids_index.setdefault(
+                    source_id, []).append(target_id)
+                target_ids.add(target_id)
+
+        target_index = {
+            target.id: target for target in await self.join.search(
+                [('id', 'in', list(target_ids))]) if target}
+
+        # List of lists
+        result = []
+        for id_ in ids:
+            targets = [target_index.get(target_id, [])
+                       for target_id in ids_index.get(id_, [])]
+            result.append(targets)
+
+        return result
--- tests/core/common/dataloader/test_joiner.py
+++ tests/core/common/dataloader/test_joiner.py
@@ -8,8 +8,7 @@ from integrark.core import StandardDataLoader, Joiner
 
 @fixture
 def join_repository():
-    repository = MemoryRepository()
-    repository.load({
+    return MemoryRepository().load({
         'default': {
             '001': SN(id='001', name='alpha', reference='x'),
             '002': SN(id='002', name='beta', reference='x'),
@@ -17,21 +16,16 @@ def join_repository():
         }
     })
 
-    return repository
 
-
-# @fixture
-# def link_repository():
-    # repository = MemoryRepository()
-    # repository.load({
-        # 'default': {
-            # '001': SN(id='001', name='alpha'),
-            # '002': SN(id='002', name='beta'),
-            # '003': SN(id='003', name='gamma'),
-        # }
-    # })
-
-    # return repository
+@fixture
+def link_repository():
+    return MemoryRepository().load({
+        'default': {
+            '001': SN(id='001', color='blue', letter_id='001'),
+            '002': SN(id='002', color='blue', letter_id='002'),
+            '003': SN(id='003', color='red', letter_id='003'),
+        }
+    })
 
 
 def test_join_dataloader_instantiation(join_repository):
@@ -74,22 +68,57 @@ async def test_join_dataloader_build_many_to_one(join_repository):
 
 
 async def test_join_dataloader_build_one_to_many(join_repository):
-    joiner = Joiner(join_repository)
-    joiner.target = 'reference'
+    joiner = Joiner(join_repository, 'reference')
 
     dataloader = joiner.build()
 
     future_1 = dataloader.load('x')
+    future_2 = dataloader.load('z')
 
-    assert len(dataloader.queue) == 1
+    assert len(dataloader.queue) == 2
     assert future_1.done() is False
+    assert future_2.done() is False
 
     await sleep(0.01)
 
     assert len(dataloader.queue) == 0
     assert future_1.done() is True
+    assert future_2.done() is True
 
     item_1 = future_1.result()
+    item_2 = future_2.result()
 
     assert item_1[0].name == 'alpha'
     assert item_1[1].name == 'beta'
+    assert item_2 == []
+
+
+async def test_join_dataloader_build_many_to_many(
+    join_repository, link_repository):
+
+    joiner = Joiner(
+        join_repository, 'letter_id', link_repository, 'color')
+
+    dataloader = joiner.build()
+
+    future_1 = dataloader.load('blue')
+    future_2 = dataloader.load('red')
+
+    assert len(dataloader.queue) == 2
+    assert future_1.done() is False
+    assert future_2.done() is False
+
+    await sleep(0.01)
+
+    assert len(dataloader.queue) == 0
+    assert future_1.done() is True
+    assert future_2.done() is True
+
+    item_1 = future_1.result()
+    item_2 = future_2.result()
+
+    assert len(item_1) == 2
+    assert item_1[0].name == 'alpha'
+    assert item_1[1].name == 'beta'
+    assert len(item_2) == 1
+    assert item_2[0].name == 'gamma'
